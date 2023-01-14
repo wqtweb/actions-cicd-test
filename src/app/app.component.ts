846a73d674032f70795297f7e9ff2ce16e531fe9
qitang.wen <qitang.wen@thoughtworks.com>
2023-01-14 10:42:12 +0800

    feat: update page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,24 @@
 import {Component, OnDestroy, OnInit} from '@angular/core';
+import { Key, KeyDAO } from '@irita/irita-sdk';
+
+class IritaKeyDAO implements KeyDAO {
+  async write(name: string, key: Key): Promise<void> {
+    return;
+  }
+
+  // 目前address和privKey是写死的
+  async read(name: string): Promise<Key> {
+    return {
+      address: '',
+      privKey: '',
+    };
+  }
+
+  async delete(name: string): Promise<void> {
+  }
+}
+
+const iritaKeyDAO = new IritaKeyDAO();
 
 @Component({
   selector: 'app-root',
